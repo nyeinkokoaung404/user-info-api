daff42fc73bb94bc832b18a83cca557898cd17db
4 0 4 \ 2.0 /🇲🇲 <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-01-07 01:57:57 +0630

    Update app.py

--- app.py
+++ app.py
@@ -164,8 +164,7 @@ async def get_client():
                 api_id=int(API_ID),
                 api_hash=API_HASH,
                 bot_token=BOT_TOKEN,
-                in_memory=True,
-                no_updates=True
+                in_memory=True
             )
             
             await client.start()
